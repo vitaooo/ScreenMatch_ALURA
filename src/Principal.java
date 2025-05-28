8bba44952195c7c26635064b2859d75a11561327
victor <victorhvercosag@gmail.com>
2025-05-27 23:32:39 -0300

    Paramos no vídeo 'Aproveitando o modelo para séries' segundo vídeo do tópico 3 de OO de Java no InteliJ

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
+import br.com.alura.screenmatch.modelos.Filme;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
-        int idade;
-        System.out.println("Digite a sua idade");
-        idade = leitura.nextInt();
-        System.out.println("Ele tem "+idade+" Anos");
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("Missão impossível 007");
+        meuFilme.setAnoDeLancamento(2022);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(7);
+        System.out.println("Total de avaliações: "+ meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+
 
     }
 }
